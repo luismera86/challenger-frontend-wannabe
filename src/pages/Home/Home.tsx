f1e7a8ec3216692580bb907d8a5b6ea00b9a4637
Luis Mera <luismera86@gmail.com>
2022-12-01 14:16:50 -0300

    .

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import { Col, Container, Row } from 'react-bootstrap'
 
+import { LayoutPages } from '../../components'
+
 const Home = () => {
   return (
     <Container className=' d-flex flex-column bg-black' fluid>
@@ -8,6 +10,7 @@ const Home = () => {
           <h1 className=' text-center mt-4 text-white-50 '>Bienvenidos a la API De Star Wars</h1>
         </Col>
       </Row>
+      <LayoutPages title='PERSONAJES'/>
     </Container>
   )
 }
