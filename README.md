ec8f760179fae45e77364475b00762c1701512f0
Luis Mera <luismera86@gmail.com>
2022-12-08 23:16:42 -0300

    Home and readme modified

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# challenger-frontend-wannabe
+# Technical Challenge: Frontend Software Engineering
+
+Welcome to your technical challenge.
+This challenge is to validate your ability as a Frontend Software Engineer with Wannabe.
+For this challenge, we expect you to develop an application that uses the Swapi.dev API and
+deploys it at Vercel.
+Your app must have the following:
+
+- A view where the user can see a list of the characters returned by the API: <https://swapi.dev/>
+- The view must have pagination.
+- A way to find characters such as filters or a search box.
+- A view where the user can see the detail of a specific character.
+  Must have:
+- Bootstrap or Tailwind for the UI.
+- Server-side rendering for each view in the application.
+- Reusable components with included documentation inside the code to explain the logic
+  of each component and how you make it reusable.
+- Proper use of git. Please don ́t upload your project to GitHub with just one commit; we
+  want to see how you use GitHub and how you document each change in your code.
+  Good Luck!
+
+## Tools used
+
+- Vite
+- React Js
+- Typescript
+- React Bootstrap
+- Redux Toolkit
+- Custom Hook
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,19 @@
 import { Col, Container, Row } from 'react-bootstrap';
 
-import { getCharacterDetails } from '@/redux/features/slices/characters/charactersDetailsSlice';
-import { useAppDispatch } from '@/redux';
-import { useEffect } from 'react';
-
 const Home = () => {
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-   // Todo tratar de hacer un hook que haga un get a todos los data directamente
-   
-  }, [])
-  
+ 
   return (
-    <Container className=' d-flex flex-column bg-black' fluid>
+    <Container className=' d-flex flex-column justify-content-center bg-black' fluid>
       <Row>
-        <Col xs={12}>
-          <h1 className=' text-center mt-4 text-white-50 '>Bienvenidos a la API De Star Wars</h1>
+        <Col className='d-flex flex-column justify-content-center align-content-center ' xs={12}>
+          <h1 className=' text-center mt-4 text-white-50 '>Challenger Frontend</h1>
+          <img className='w-50 align-self-center' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/800px-Star_Wars_Logo.svg.png" alt="" />
+        </Col>
+      </Row>
+      <Row>
+        <Col className='text-warning'>
+          <p>Creado por Luis Mera</p>
+          <p>Link del <a className='text-decoration-none' href="https://github.com/luismera86/challenger-frontend-wannabe">Repositorio</a></p>
         </Col>
       </Row>
     </Container>
